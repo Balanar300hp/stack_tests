ebcc149f09d44b9235585619d56e1e106c13704b
Balanar300hp <kudashov.vlad@yandex.ru>
2016-09-18 16:24:17 +0300

    Update matrix.h

--- include/matrix.h
+++ include/matrix.h
@@ -1,51 +1,21 @@
-#pragma once
-#ifndef MATR_H
-#define NATR_H
-#include "MatrixException.h"
 #include <iostream>
-#include <cstring> 
+using namespace std;
 
 template <typename T>
-class Matrix;
-
-template <class T>
-std::ostream & operator<<(std::ostream & output, const Matrix<T> &);
-
-template <class T>
-std::istream & operator>>(std::istream & input, Matrix<T> &);
-
-
-template <typename T>
-class Matrix {
-private:
-	void swap(Matrix & x);
-	T **matrix;
-	int columns;
-	int rows;
-
+class stack
+{
 public:
-	Matrix() : rows(0), columns(0), matrix(nullptr) {};	// Done
-	Matrix(unsigned int n, unsigned int m);					// Done
-	Matrix(T **matr, unsigned int _n, unsigned int _m);
-	Matrix(Matrix const & x);
-	~Matrix();
-
-	unsigned int rowsNumber() const;							// Done
-	unsigned int columnsNumber() const;							// Done
-	bool readFromFile(char *path);								// Done
-	Matrix &operator =(const Matrix& m2);
-	Matrix operator +(const Matrix& m2);						// Done
-	Matrix operator -(const Matrix& m2);						// Done
-	Matrix operator *(double num);								// Done
-	Matrix operator *(const Matrix& m2);						// Done
-	bool operator ==(const Matrix& m2);						// Done
-	T* operator [](unsigned int index);							// Done
-
-	friend std::ostream & operator<< <>(std::ostream & output, const Matrix & matrix);	// Done
-	friend std::istream & operator>> <>(std::istream & input, Matrix<T> & matrix);		// Done
+	stack();
+	stack(const stack&);
+	auto count() const noexcept->size_t;
+	auto push(T const &)->void;
+	auto pop()->T;
+	auto top() const->T&;
+	~stack();
+	auto operator=(const stack&tmp)->stack&;
+	auto print()->void;
+private:
+	T * array_;
+	size_t array_size_;
+	size_t count_;
 };
-
-
-#include "Matrix.cpp"
-
-#endif
