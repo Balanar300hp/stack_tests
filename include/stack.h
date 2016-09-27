b50a8b0846adb76ac2bc80c175d7c449f6ff7888
Balanar300hp <kudashov.vlad@yandex.ru>
2016-09-27 21:21:09 +0300

    Update stack.h

--- include/stack.h
+++ include/stack.h
@@ -12,11 +12,12 @@ protected:
 	size_t size_;
 	size_t count_;
 };
+__________________________________________________________________________________________________________________
+__________________________________________________________________________________________________________________
+
 
 template <typename T>// конструктор аллокатора
 inline allocator<T>::allocator(size_t size) : ptr_(static_cast<T *>(size == 0 ? nullptr : operator new(size * sizeof(T)))), size_(0), count_(size) {
-
-	std::cout << "ZDES ALLOCATOR CONSTRUCTOR & size=" <<count_<< std::endl;
 };
 
 template <typename T>//деструктор аллокатора
