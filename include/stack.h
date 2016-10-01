ad578c08bb309fd2fe884a788842c004b2b24366
Balanar300hp <kudashov.vlad@yandex.ru>
2016-10-01 22:07:35 +0300

    Update stack.h

--- include/stack.h
+++ include/stack.h
@@ -7,8 +7,8 @@ public:
 	stack(stack const &); /*strong*/
 	auto count() const noexcept->size_t;/*noexcept*/
 	auto push(T const &)->void;/*strong*/
-	auto pop()->T;/*basic*/
-	//auto top() const->T&;/*strong*/
+	auto pop()->T;/*strong*/
+	auto top() const->T&;/*strong*/
 	~stack(); 	/*noexcept*/
 	auto operator=(const stack&tmp)->stack&;/*strong*/
 	auto empty()->bool;/*noexcept*/
