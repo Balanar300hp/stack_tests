0118a6d531b512a50106b0c88b8f4f2c7946ad1d
Balanar300hp <kudashov.vlad@yandex.ru>
2016-10-01 21:31:30 +0300

    Update stack.h

--- include/stack.h
+++ include/stack.h
@@ -1,34 +1,15 @@
-#include <iostream>
-template<typename T>
-class allocator
+#include "stack.h"
+template <typename T>
+class stack 
 {
-protected:
-	allocator(size_t size = 0);
-	~allocator();
-	auto swap(allocator& other)->void;
-	allocator(allocator const&) = delete;
-	auto operator=(allocator const&)->allocator& = delete;
-	T * ptr_;
-	size_t size_;
-	size_t count_;
-};
-//__________________________________________________________________________________________________________________
-//__________________________________________________________________________________________________________________
-
-
-template <typename T>// конструктор аллокатора
-inline allocator<T>::allocator(size_t size) : ptr_(static_cast<T *>(size == 0 ? nullptr : operator new(size * sizeof(T)))), size_(0), count_(size) {
-};
-
-template <typename T>//деструктор аллокатора
-inline allocator<T>::~allocator(){
-
-delete static_cast<void*>(ptr_);
-};
-
-template <typename T>//swap allocator
-inline auto allocator<T>::swap(allocator& other)->void {
-	std::swap(ptr_,other.ptr_);
-	std::swap(size_, other.size_);
-	std::swap(count_, other.count_);
+public:
+	stack();/*noexcept*/
+	stack(stack const &); /*strong*/
+	auto count() const noexcept->size_t;/*noexcept*/
+	auto push(T const &)->void;/*strong*/
+	auto pop()->T;/*strong*/
+	auto top() const->T&;/*strong*/
+	~stack(); 	/*noexcept*/
+	auto operator=(const stack&tmp)->stack&;/*strong*/
+	auto empty()->bool;/*noexcept*/
 };
