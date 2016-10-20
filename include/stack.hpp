32210adadb91dcdd04c7399c7de46584abb67c33
Balanar300hp <kudashov.vlad@yandex.ru>
2016-10-20 13:31:47 +0300

    Update stack.hpp

--- include/stack.hpp
+++ include/stack.hpp
@@ -2,13 +2,45 @@
 #include <stdexcept>
 
 
+template<typename T>
+class allocator
+{
+protected:
+	allocator(size_t size = 0);
+	~allocator();
+	auto swap(allocator& other)->void;
+	allocator(allocator const&) = delete;
+	auto operator=(allocator const&)->allocator& = delete;
+	T * ptr_;
+	size_t size_;
+	size_t count_;
+};
 
+//__________________________________________________________________________________________________________________
+//__________________________________________________________________________________________________________________
+
+
+template <typename T>// êîíñòðóêòîð àëëîêàòîðà
+allocator<T>::allocator(size_t size) : ptr_(static_cast<T *>(size == 0 ? nullptr : operator new(size * sizeof(T)))), size_(0), count_(size) {
+};
+
+template <typename T>//äåñòðóêòîð àëëîêàòîðà
+allocator<T>::~allocator() {
+operator delete(ptr_);
+};
+
+template <typename T>//swap allocator
+auto allocator<T>::swap(allocator& other)->void {
+	std::swap(ptr_, other.ptr_);
+	std::swap(size_, other.size_);
+	std::swap(count_, other.count_);
+};
 
 //__________________________________________________________________________________________________________________
 //__________________________________________________________________________________________________________________
  
 template <typename T>
-class stack allocator<T>
+class stack : private allocator<T>
 {
 public:
 	stack();/*noexcept*/
@@ -30,7 +62,7 @@ public:
 
 template<typename T> // ïðîâåðêà íà ïóñòîòó ñòýêà 
 auto stack<T>::empty()->bool {
-	return (count_ == 0);
+	return (allocator<T>::count_ == 0);
 }
 
 template <typename T>//êîïèðîâàíèå è âûäåëåíèå ïàìÿòè 
@@ -53,23 +85,23 @@ stack<T>::stack()  {
 
 template <typename T>//âñòàâêà ýëåìåíòà â ñòýê 
 auto stack<T>::push(T const &val)->void {
-	if count_ == size_) {//óâåëè÷èâàåò ïàìÿòü 
+	if (allocator<T>::count_ == allocator<T>::size_) {//óâåëè÷èâàåò ïàìÿòü 
 		size_t size = allocator<T>::size_ * 2 + (allocator<T>::size_ == 0);
-		T *tmp = mem_copy(count_, size, ptr_);
-		delete[] ptr_;
-		ptr_ = tmp;
-		size_ = size;
+		T *tmp = mem_copy(allocator<T>::count_, size, allocator<T>::ptr_);
+		delete[] allocator<T>::ptr_;
+		allocator<T>::ptr_ = tmp;
+		allocator<T>::size_ = size;
 	}
 	
-	++count_;
+	++allocator<T>::count_;
 }
 
 
 template <typename T>//êîíñòðóêòîð êîïèðîâàíèÿ
 stack<T>::stack(stack const &tmp) {
-	count_ = tmp.count_;
-	ptr_ = mem_copy(tmp.count_, tmp.size_, tmp.ptr_);
-	size_ = tmp.size_;
+	allocator<T>::count_ = tmp.count_;
+	allocator<T>::ptr_ = mem_copy(tmp.count_, tmp.size_, tmp.ptr_);
+	allocator<T>::size_ = tmp.size_;
 };
 
 template <typename T>//ïåðåãðóçêà îïåðàòîðà ïðèñâàèâàíèÿ 
@@ -77,9 +109,9 @@ auto stack<T>::operator=(const stack &tmp)->stack& {
 	if (this != &tmp) {
 		T* cp = mem_copy(tmp.count_, tmp.size_, tmp.ptr_);
 		delete[] allocator<T>::ptr_; 
-		ptr_ = cp;
-		size_ =tmp.size_;
-		count_ = tmp.count_;
+		allocator<T>::ptr_ = cp;
+		allocator<T>::size_ =tmp.size_;
+		allocator<T>::count_ = tmp.count_;
 	}
 	return *this;
 }
@@ -87,19 +119,19 @@ auto stack<T>::operator=(const stack &tmp)->stack& {
 
 template <typename T>//âîçâðàùàåì count_
 auto stack<T>::count() const noexcept->size_t {
-	return count_;
+	return allocator<T>::count_;
 }
 
 template <typename T>// óìåíüøåíèå count_ 
 auto stack<T>::pop()->void {
-	if (count_ == 0) throw std::logic_error("Empty!");
+	if (allocator<T>::count_ == 0) throw std::logic_error("Empty!");
 	
-	--count_;
+	--allocator<T>::count_;
 }
 
 template <typename T>//óäàëåíèå ýëåìåíòà
 auto stack<T>::top() const->T& {
-	if (count_ == 0) throw std::logic_error("Empty!");
-	return ptr_[count_-1];
+	if (allocator<T>::count_ == 0) throw std::logic_error("Empty!");
+	return allocator<T>::ptr_[allocator<T>::count_-1];
 
 }
