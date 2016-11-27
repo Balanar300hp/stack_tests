2f8effded0a0b877816f9dc81990a152d47316b2
Balanar300hp <kudashov.vlad@yandex.ru>
2016-11-27 17:35:52 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -68,10 +68,10 @@ SCENARIO("mutex", "[mutex]"){
   s1.push(1);
   s1.push(2);
   
-  thread potok_1(&stack<int>::push, &stack, 2);
+  thread potok_1(&stack<int>::push, &s1, 2);
   potok_1.join();
   REQUIRE(s.top()==2);
-  thread potok_2(&stack<int>::pop, &stack);
+  thread potok_2(&stack<int>::pop, &1);
   potok_2.join();
   REQUIRE(s.top()==1);
 }
